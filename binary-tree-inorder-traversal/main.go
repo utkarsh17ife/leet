c2b47610f3e3a6da9fcd3d40f37cfe1e87f57675
utkarsh-dev <utkarsh17ife@gmail.com>
2021-08-19 09:40:57 +0530

    fix: binary tree treversal

--- binary-tree-inorder-traversal/main.go
+++ binary-tree-inorder-traversal/main.go
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/binary-tree-inorder-traversal/
 package main
 
+import "fmt"
+
 /**
  * Definition for a binary tree node.
  */
@@ -11,10 +13,30 @@ type TreeNode struct {
 }
 
 func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
 
-	return nil
+	arr := make([]int, 0)
+	arr = append(arr, inorderTraversal(root.Left)...)
+	arr = append(arr, root.Val)
+	arr = append(arr, inorderTraversal(root.Right)...)
+
+	return arr
 }
 
 func main() {
+
+	root := &TreeNode{40, nil, nil}
+	root.Left = &TreeNode{20, nil, nil}
+	root.Left.Left = &TreeNode{10, nil, nil}
+	root.Left.Left.Left = &TreeNode{5, nil, nil}
+	root.Left.Right = &TreeNode{30, nil, nil}
+	root.Right = &TreeNode{50, nil, nil}
+	root.Right.Right = &TreeNode{60, nil, nil}
+	root.Left.Right.Left = &TreeNode{67, nil, nil}
+	root.Left.Right.Right = &TreeNode{78, nil, nil}
+
+	fmt.Println(inorderTraversal(root))
 
 }
