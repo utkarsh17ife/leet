43cdd05d70271d27f8e824dbeff8c84489f1367d
utkarsh-dev <utkarsh17ife@gmail.com>
2021-08-22 14:23:44 +0530

    fix: palindrom ll start

---
+++ palindrome-linked-list/main.go
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+package main
+
+/**
+ * Definition for singly-linked list.
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+
+	return false
+}
+
+func main() {
+
+}
